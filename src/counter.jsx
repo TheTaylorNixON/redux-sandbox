f1e646ddfd68cfe5aab53b69902f54cc0ffef792
theTylorNixon <bochkarevtrade@gmail.com>
2019-06-04 22:56:55 +0300

    bindActionCreators, рендер counter, рефакторинг

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Counter = () => {
+const Counter = ({ counter, inc, dec, rnd }) => {
     return (
         <div id="root" className="jumbotron">
-            <h2>0</h2>
-            <button>INC</button>
-            <button>DEC</button>
-            <button>RND</button>
+            <h2>{counter}</h2>
+            <button onClick={inc}>INC</button>
+            <button onClick={dec}>DEC</button>
+            <button onClick={rnd}>RND</button>
         </div>
-    )
-}
+    );
+};
 
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux'; 
+import { createStore, bindActionCreators } from 'redux';
 
 import reducer from './reducer';
 import * as actions from './actions';
 
+import Counter from './counter';
 
-const { inc, dec, rnd } = actions;
+
 const store = createStore(reducer);
+const { dispatch } = store;
 
-store.dispatch(inc());
-store.dispatch(rnd());
-store.dispatch(dec());
+// нативный dispatch
+// const { inc, dec, rnd } = actions;
+// dispatch(inc());
+// dispatch(rnd());
+// dispatch(dec());
+// store.subscribe(() => {
+//     console.log(store.getState());
+// })
 
-store.subscribe(() => {
-    console.log(store.getState());
-})
+
+// самописный bindActionCreator
+// const bindActionCreator = (creator, dispatch) => ( ...args) => {
+//     dispatch(creator( ...args));
+// }
+// bindActionCreator(inc, dispatch);
+// bindActionCreator(dec, dispatch);
+// bindActionCreator(rnd, dispatch);
+
+
+// оригинальная bindActionCreators от redux
+// const { incDispatch, decDispatch, rndDispatch } = bindActionCreators({
+//     incDispatch: inc,
+//     decDispatch: dec,
+//     rndDispatch: rnd
+// }, dispatch)
+
+
+// рефакторинг оригинальной bindActionCreators
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+const update = () => {
+    ReactDOM.render(
+        <Counter
+            counter={store.getState()}
+            inc={inc}
+            dec={dec}
+            rnd={() => {
+                const value = Math.ceil(Math.random() * 10);
+                console.log(value);
+                rnd(value);
+            }}
+        />,
+        document.getElementById('root'));
+}
+
+update();
+store.subscribe(update);

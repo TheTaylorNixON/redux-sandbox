c246227e2576d755f981e4ee541dc1a6588f034b
bochkarev.av <bochkarev.av@net868.ru>
2019-06-04 15:28:41 +0300

    добавил reducer, actions, counter

---
+++ src/counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div id="root" className="jumbotron">
+            <h2>0</h2>
+            <button>INC</button>
+            <button>DEC</button>
+            <button>RND</button>
+        </div>
+    )
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'; 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import reducer from './reducer';
+import * as actions from './actions';
+
+
+const { inc, dec, rnd } = actions;
+const store = createStore(reducer);
+
+store.dispatch(inc());
+store.dispatch(rnd());
+store.dispatch(dec());
+
+store.subscribe(() => {
+    console.log(store.getState());
+})
